451f1dcf03a12f0b1d52c0198465089abae9879e
Andrés Rodríguez <andres.rodriguezv05@gmail.com>
2024-02-10 12:48:42 -0400

    chat

--- app.js
+++ app.js
@@ -1,54 +1,67 @@
-const express = require('express')
-const json = require('./Mocks/HorrorMovies.json')
-const pc = require('picocolors')
-const { validMovie } = require('./modules/MoviesSchemas')
+import express from 'express'
+import pc from 'picocolors'
+import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+import { argv } from 'node:process'
+
+dotenv.config()
 const app = express()
-const port = process.env.PORT ?? 3000
+const serverHTTP = createServer(app)
+const io = new Server(serverHTTP)
+const PORT = process.env.PORT ?? '3000'
+const db = createClient({
+  url: 'libsql://my-db-andreserl2504.turso.io',
+  authToken: process.env.DB_TOKEN
+})
 
-app.disable('x-powered-by')
-app.use(express.static('public'))
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT
+  )`)
 
+io.on('connection', async (socket) => {
+  console.log(pc.green('An user has connected'))
+  socket.on('chat message', async (msg) => {
+    let result
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content) VALUES(:content)',
+        args: { content: msg }
+      })
+    } catch (e) {
+      console.error(e)
+      return
+    }
+    io.emit('chat message', msg, result.lastInsertRowid.toString())
+  })
+  socket.on('disconnect', () => {
+    console.log(pc.yellow('An user has disconnected'))
+  })
 
-app.get('/movies', (req, res) => {
-  const { title } = req.query
-  if (title) {
-    const movieJSON = json.Search.filter((movie) => movie.Title.includes(title))
-    return res.json({
-      Search: movieJSON,
-      totalResults: movieJSON.length,
-      Response: true
-    })
+  if (!socket.recovered) {
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content FROM messages WHERE id > ?',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+      results.rows.forEach(row => {
+        socket.emit('chat message', row.content, row.id.toString())
+      })
+    } catch (e) {
+      console.error(e)
+    }
   }
-  res.json(json)
 })
-app.get('/movies/:id', (req, res) => {
-  const { id } = req.params
-  if (json.Search[id - 1]) {
-    const movieJSON = json.Search[id - 1]
-    return res.json({
-      Search: movieJSON,
-      totalResults: movieJSON.length,
-      Response: true
-    })
-  }
-
-  res.status(404).json({ Message: 'Not Found', Response: false })
+app.use(logger('dev'))
+app.get('/', (req, res) => {
+  res.sendFile(process.cwd() + '/client/index.html')
 })
 
-app.post('/movies', (req, res) => {
-  const result = validMovie(req.body)
-
-  if (result.error) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) })
-  }
-  json.Search.push(result)
-  res.status(201).json(result.data)
-})
-
-app.use((req, res) => {
-  res.status(404).json({ Error: '404', Message: 'Not Found' })
-})
-
-app.listen(port, () => {
-  console.log(pc.blue(`App listening on port http://localhost:${port}`))
+serverHTTP.listen(PORT, () => {
+  console.log(pc.bgBlue('Server running 😸😸'))
+  console.log(pc.blue(`http://localhost:${PORT}`))
 })
